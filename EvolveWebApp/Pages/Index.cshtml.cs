d200bf311f3f3adea5e2b4e7f0c779a909de48e2
satish.rathore <satish.rathore@gmail.com>
2022-12-27 12:08:45 +0530

    Add Azure AD Auth

--- EvolveWebApp/Pages/Index.cshtml.cs
+++ EvolveWebApp/Pages/Index.cshtml.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Graph;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -18,11 +21,15 @@ namespace EvolveWebApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly GraphServiceClient _graphServiceClient;
+        private readonly IConfiguration _configuration;
         private AppSettings _appSettings { get; set; }
-        public IndexModel(ILogger<IndexModel> logger, IOptions<AppSettings> settings)
+        public IndexModel(ILogger<IndexModel> logger,
+            IOptions<AppSettings> settings, IConfiguration configuration)
         {
             _logger = logger;
-            _appSettings = settings.Value;           
+            _appSettings = settings.Value;
+            _configuration = configuration;
         }
 
         public void OnGet()
@@ -35,40 +42,55 @@ namespace EvolveWebApp.Pages
         {
             ViewData["successMsg"] = null;
             ViewData["UserRequest"] = null;
-            if (!string.IsNullOrEmpty(employeeID))
-            {
-                var Url = _appSettings.AzureFunctionURL;
 
-                dynamic content = new { employeeId = employeeID };
+            string tenantID = _configuration.GetValue<string>("AzureAD:TenantID");
+            string clientID = _configuration.GetValue<string>("AzureAD:ClientID");
+            string clientSecret = _configuration.GetValue<string>("AzureAD:ClientSecret");
+            ClientSecretCredential clientSecretCred = new ClientSecretCredential(tenantID, clientID, clientSecret);
+            GraphServiceClient graphServiceClientobj = new GraphServiceClient(clientSecretCred);
 
-                //CancellationToken cancellationToken;
+            //var user = graphServiceClientobj.Users.Request().Select(x => x.DisplayName).GetAsync().Result;
+
+            var user = await graphServiceClientobj.Users["1000038829"]
+                    .Request()
+                    .GetAsync();
+
+            return Page();
+
+            //if (!string.IsNullOrEmpty(employeeID))
+            //{
+            //    var Url = _appSettings.AzureFunctionURL;
+
+            //    dynamic content = new { employeeId = employeeID };
+
+            //    //CancellationToken cancellationToken;
 
 
-                using (var client = new HttpClient())
-                using (var request = new HttpRequestMessage(HttpMethod.Post, Url))
-                using (var httpContent = CreateHttpContent(content))
-                {
-                    request.Content = httpContent;
+            //    using (var client = new HttpClient())
+            //    using (var request = new HttpRequestMessage(HttpMethod.Post, Url))
+            //    using (var httpContent = CreateHttpContent(content))
+            //    {
+            //        request.Content = httpContent;
 
-                    using (var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
-                        .ConfigureAwait(false))
-                    {
+            //        using (var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
+            //            .ConfigureAwait(false))
+            //        {
 
-                        var resualtList = response.Content.ReadAsAsync<List<UserRequest>>();
+            //            var resualtList = response.Content.ReadAsAsync<List<UserRequest>>();
 
 
-                        ViewData["UserRequest"] = resualtList.Result;
+            //            ViewData["UserRequest"] = resualtList.Result;
 
-                        return Page();
-                    }
-                }
-            }
-            else
-            {
-                ViewData["UserRequest"] = null;
-                ViewData["successMsg"] = "Lab created successfully, An email was sent with instructions for accessing lab !!";
-                return Page();
-            }
+            //            return Page();
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    ViewData["UserRequest"] = null;
+            //    ViewData["successMsg"] = "Lab created successfully, An email was sent with instructions for accessing lab !!";
+            //    return Page();
+            //}
 
         }
 
--- EvolveWebApp/Startup.cs
+++ EvolveWebApp/Startup.cs
@@ -8,6 +8,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Identity.Web;
+using Microsoft.Graph;
+using Azure.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace EvolveWebApp
 {
@@ -25,6 +30,24 @@ namespace EvolveWebApp
         {
             services.AddRazorPages();
             services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+            //services.AddMicrosoftIdentityWebApiAuthentication(Configuration, "AzureAd").
+            //    EnableTokenAcquisitionToCallDownstreamApi().
+            //    AddInMemoryTokenCaches().
+            //    AddMicrosoftGraph(x =>
+            //    {
+            //        string tenantID = Configuration.GetValue<string>("AzureAD:TenantID");
+            //        string clientID = Configuration.GetValue<string>("AzureAD:ClientID");
+            //        string clientSecret = Configuration.GetValue<string>("AzureAD:ClientSecret");
+            //        ClientSecretCredential clientSecretCred = new ClientSecretCredential(tenantID, clientID, clientSecret);
+            //        return new GraphServiceClient(clientSecretCred);
+            //    },
+            //    new string[] { ".default" });
+
+            //services.AddMvc(options =>
+            //{
+            //    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+            //    options.Filters.Add(new AuthorizeFilter(policy));
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +68,7 @@ namespace EvolveWebApp
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            //app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
